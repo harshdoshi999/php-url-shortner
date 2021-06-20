bd7f5b6391b4071bfa86cdce020633bb822bf480
Hrs Doshi <harshdoshi999@users.noreply.github.com>
2021-06-20 18:36:05 +0530

    Add files via upload

--- src/UrlShortener.php
+++ src/UrlShortener.php
@@ -25,7 +25,17 @@ class UrlShortener
      */
     public function shorten($url)
     {
-        $url = "test.com";
-        return $url;
+        $curl = curl_init();
+
+        // set our url with curl_setopt()
+        curl_setopt($curl, CURLOPT_URL, "https://cdpt.in/shorten?url=".$url);
+
+        // return the transfer as a string, also with setopt()
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        // curl_exec() executes the started curl session
+        // $output contains the output string
+        $output = curl_exec($curl);
+        return $output;
     }
 }
